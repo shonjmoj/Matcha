d8c4832c757476e9f010a6bd586ae69f04850c4f
Miid0 <midoounejjart@gmail.com>
2022-06-23 16:11:14 +0100

    integrating framer motion and working on landing page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,17 +3,22 @@ module.exports = {
   darkMode: "class",
   content: ["./pages/**/*.{tsx, ts, jsx, js}"],
   theme: {
-    colors: {
-      pink: "#FF83CE",
-      "soft-pink": "#FF8AAF",
-      orange: "#FF9D8F",
-      "soft-orange": "#FFBA74",
-      yellow: "#FFD967",
-      "soft-yellow": "#F9F871",
-      white: "#FFFFFF",
-      black: "#000000",
+    extend: {
+      colors: {
+        pink: "#FF83CE",
+        "soft-pink": "#FF8AAF",
+        orange: "#FF9D8F",
+        "soft-orange": "#FFBA74",
+        yellow: "#FFD967",
+        "soft-yellow": "#F9F871",
+        white: "#FFFFFF",
+        black: "#000000",
+        "dark-gray": "#0F172A",
+        gray: "#34334D",
+        "pinky-dark-gray": "#BE91B7",
+        "pinky-gray": "#f3B4D7",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
