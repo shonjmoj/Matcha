cac2a3abc401712ee5b38384d2c6b72eb8ef0179
aymane <aymensarrar1@gmail.com>
2022-06-25 18:31:31 +0100

    added authentication with email confirmation

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { motion } from "framer-motion";
-import { TiHeartFullOutline } from "react-icons/ti";
 import Link from "next/link";
 import Mode from "../components/Mode";
 
 const Home: NextPage = () => {
   return (
-    <motion.div className="text-white flex flex-col items-center w-full">
+    <motion.div className="flex flex-col items-center w-full text-white">
       <motion.h1
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1.1 }}
         transition={{ delay: 0.5 }}
-        className="text-5xl md:text-6xl lg:text-7xl font-semibold"
+        className="text-5xl font-semibold md:text-6xl lg:text-7xl"
       >
         Matcha
       </motion.h1>
@@ -27,7 +26,7 @@ const Home: NextPage = () => {
         weather.Because you deserve what dating deserves: better.
       </motion.p>
       <motion.button
-        className="py-2 px-3 mt-8 text-sm lg:text-lg border-white border-2 shadow-xs hover:shadow-sm hover:shadow-white transition-shadow duration-200 shadow-white/50"
+        className="px-3 py-2 mt-8 text-sm transition-shadow duration-200 border-2 border-white shadow-xs lg:text-lg hover:shadow-sm hover:shadow-white shadow-white/50"
         initial={{ x: -600, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         whileTap={{
