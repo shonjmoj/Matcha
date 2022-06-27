7f9d7e90daaca3a11e8010a76de29c6fba06b0f2
Miid0 <elounejj@student.1337.ma>
2022-06-27 19:14:47 +0100

    setuping redux store

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,9 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { motion } from 'framer-motion';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
 
 export default MyApp;
