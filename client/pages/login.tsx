5b109b8955d6dd4cd1b4586f29c9a77dc5e0829f
Miid0 <elounejj@student.1337.ma>
2022-06-27 13:12:44 +0100

    catching data from the login and registration forms using react-form hook

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,51 +1,58 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 export default function Login() {
-	return (
-		<motion.div
-			className='text-white container w-full sm:w-[50%]'
-			initial={{ x: -600, opacity: 0 }}
-			animate={{ x: 0, opacity: 1 }}
-			transition={{ delay: 0.5 }}>
-			<motion.h1
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ delay: 1.5 }}
-				className='text-2xl xl:text-4xl text-center mb-8 xl:mb-12 font-semibold'>
-				It is never too late <br /> to fall in love
-			</motion.h1>
-			<form className='flex flex-col gap-2 items-center'>
-				<label className='flex flex-col font-semibold'>
-					Username / email
-					<input
-						type='text'
-						className='bg-transparent border-white font-normal border-[1px] outline-none p-2'
-					/>
-				</label>
-				<label className='flex flex-col font-semibold'>
-					Password
-					<input
-						type='password'
-						className='bg-transparent border-white font-normal border-[1px] outline-none p-2'
-					/>
-				</label>
-				<span className='text-sm lg:text-md font-light'>
-					You are new here ?{' '}
-					<span className='underline'>
-						<Link href='/registration'>Create an account</Link>
-					</span>
-				</span>
-				<motion.button
-					className='mt-2 border-2 px-20 py-2 shadow-xs hover:shadow-sm hover:shadow-white transition-all duration-200 shadow-white/50'
-					whileTap={{
-						scale: 1,
-					}}
-					whileHover={{ scale: 1.1 }}>
-					Login
-				</motion.button>
-			</form>
-		</motion.div>
-	);
+  const { register, handleSubmit } = useForm();
+  return (
+    <motion.div
+      className='text-white container w-full sm:w-[50%]'
+      initial={{ x: -600, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ delay: 0.5 }}>
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.5 }}
+        className='text-2xl xl:text-4xl text-center mb-8 xl:mb-12 font-semibold'>
+        It is never too late <br /> to fall in love
+      </motion.h1>
+      <form
+        className='flex flex-col gap-2 items-center'
+        onSubmit={handleSubmit((data) => console.log(data))}>
+        <label className='flex flex-col font-semibold'>
+          Username / email
+          <input
+            type='text'
+            {...register('username / email')}
+            className='bg-transparent border-white font-normal border-[1px] outline-none p-2'
+          />
+        </label>
+        <label className='flex flex-col font-semibold'>
+          Password
+          <input
+            type='password'
+            {...register('password')}
+            className='bg-transparent border-white font-normal border-[1px] outline-none p-2'
+          />
+        </label>
+        <span className='text-sm lg:text-md font-light'>
+          You are new here ?{' '}
+          <span className='underline'>
+            <Link href='/registration'>Create an account</Link>
+          </span>
+        </span>
+        <motion.button
+          type='submit'
+          className='mt-2 border-2 px-20 py-2 shadow-xs hover:shadow-sm hover:shadow-white transition-all duration-200 shadow-white/50'
+          whileTap={{
+            scale: 1,
+          }}
+          whileHover={{ scale: 1.1 }}>
+          Login
+        </motion.button>
+      </form>
+    </motion.div>
+  );
 }
--- client/pages/registration.tsx
+++ client/pages/registration.tsx
@@ -1,72 +1,84 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { useForm } from 'react-hook-form';
 
 export default function registration() {
-	return (
-		<motion.div
-			className='text-white container w-full sm:w-[30%]'
-			initial={{ x: -600, opacity: 0 }}
-			animate={{ x: 0, opacity: 1 }}
-			transition={{ delay: 0.5 }}>
-			<motion.h1
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ delay: 1.5 }}
-				className='text-2xl xl:text-4xl text-center mb-4 xl:mb-8 font-semibold mx-2'>
-				Get noticed for who you are, not what you look like.
-			</motion.h1>
-			<form className='grid lg:grid-cols-2 justify-center items-center gap-3'>
-				<label className='flex flex-col font-semibold'>
-					First name
-					<input
-						type='text'
-						className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
-					/>
-				</label>
-				<label className='flex flex-col font-semibold'>
-					Last name
-					<input
-						type='text'
-						className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
-					/>
-				</label>
-				<label className='flex flex-col font-semibold lg:col-span-2'>
-					Username
-					<input
-						type='text'
-						className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
-					/>
-				</label>
-				<label className='flex flex-col font-semibold lg:col-span-2'>
-					email
-					<input
-						type='email'
-						className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
-					/>
-				</label>
-				<label className='flex flex-col font-semibold'>
-					Password
-					<input
-						type='password'
-						className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
-					/>
-				</label>
-				<label className='flex flex-col font-semibold'>
-					Confirm Password
-					<input
-						type='password'
-						className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
-					/>
-				</label>
-				<motion.button
-					className='mt-2 border-2 p-1 lg:p-2 shadow-xs hover:shadow-sm hover:shadow-white transition-all duration-200 shadow-white/50 lg:col-span-2'
-					whileTap={{
-						scale: 1,
-					}}
-					whileHover={{ scale: 1.1 }}>
-					Register
-				</motion.button>
-			</form>
-		</motion.div>
-	);
+  const { register, handleSubmit } = useForm();
+  return (
+    <motion.div
+      className='text-white container w-full sm:w-[30%]'
+      initial={{ x: -600, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ delay: 0.5 }}>
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.5 }}
+        className='text-2xl xl:text-4xl text-center mb-4 xl:mb-8 font-semibold mx-2'>
+        Get noticed for who you are, not what you look like.
+      </motion.h1>
+      <form
+        className='grid lg:grid-cols-2 justify-center items-center gap-3'
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}>
+        <label className='flex flex-col font-semibold'>
+          First name
+          <input
+            type='text'
+            {...register('firstname')}
+            className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
+          />
+        </label>
+        <label className='flex flex-col font-semibold'>
+          Last name
+          <input
+            type='text'
+            {...register('lastname')}
+            className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
+          />
+        </label>
+        <label className='flex flex-col font-semibold lg:col-span-2'>
+          Username
+          <input
+            type='text'
+            {...register('username')}
+            className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
+          />
+        </label>
+        <label className='flex flex-col font-semibold lg:col-span-2'>
+          email
+          <input
+            type='email'
+            {...register('email')}
+            className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
+          />
+        </label>
+        <label className='flex flex-col font-semibold'>
+          Password
+          <input
+            type='password'
+            {...register('password')}
+            className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
+          />
+        </label>
+        <label className='flex flex-col font-semibold'>
+          Confirm Password
+          <input
+            type='password'
+            className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
+          />
+        </label>
+        <motion.button
+          type='submit'
+          className='mt-2 border-2 p-1 lg:p-2 shadow-xs hover:shadow-sm hover:shadow-white transition-all duration-200 shadow-white/50 lg:col-span-2'
+          whileTap={{
+            scale: 1,
+          }}
+          whileHover={{ scale: 1.1 }}>
+          Register
+        </motion.button>
+      </form>
+    </motion.div>
+  );
 }
