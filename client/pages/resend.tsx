0f2b112611aaab6840fcbbdb71b1af2b6075ad2c
aymane <aymensarrar1@gmail.com>
2022-06-27 18:32:06 +0100

    validating for resend form

--- client/pages/resend.tsx
+++ client/pages/resend.tsx
@@ -1,35 +1,53 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-
-const resendEmail = () => {
+import React from "react";
+import { motion } from "framer-motion";
+import Joi from "joi";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ResendData } from "../types/types";
+const ResendEmail = () => {
+  const { register, handleSubmit } = useForm<ResendData>();
+  const schema = Joi.object({
+    email: Joi.string().email({ tlds: { allow: false } }),
+  });
+  const onSubmit: SubmitHandler<ResendData> = (data) => {
+    const { error, value } = schema.validate(data);
+    if (!error) {
+    }
+  };
   return (
     <motion.div
-      className='text-white container w-full sm:w-[30%]'
+      className="text-white container w-full sm:w-[30%]"
       initial={{ x: -600, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
-      transition={{ delay: 0.5 }}>
+      transition={{ delay: 0.5 }}
+    >
       <motion.h1
         initial={{ opacity: 0, x: -200 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ delay: 0.6 }}
-        className='text-xl md:text-2xl mb-4 xl:text-4xl font-bold'>
+        className="mb-4 text-xl font-bold md:text-2xl xl:text-4xl"
+      >
         Retype the email
       </motion.h1>
-      <form className='w-full flex flex-col gap-2'>
-        <label className='flex flex-col font-semibold'>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col w-full gap-2"
+      >
+        <label className="flex flex-col font-semibold">
           Email
           <input
-            type='email'
-            className='bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none'
+            {...register("email", { required: true })}
+            type="email"
+            className="bg-transparent border-[1px] p-1 font-normal lg:p-2 outline-none"
           />
         </label>
         <motion.button
-          type='submit'
-          className='p-1 mt-2 transition-all duration-200 border-2 shadow-xs lg:p-2 hover:shadow-sm hover:shadow-white shadow-white/50 lg:col-span-2'
+          type="submit"
+          className="p-1 mt-2 transition-all duration-200 border-2 shadow-xs lg:p-2 hover:shadow-sm hover:shadow-white shadow-white/50 lg:col-span-2"
           whileTap={{
             scale: 1,
           }}
-          whileHover={{ scale: 1.1 }}>
+          whileHover={{ scale: 1.1 }}
+        >
           Resend
         </motion.button>
       </form>
@@ -37,4 +55,4 @@ const resendEmail = () => {
   );
 };
 
-export default resendEmail;
+export default ResendEmail;
--- client/types/types.ts
+++ client/types/types.ts
@@ -1,4 +1,15 @@
 export type LoginData = {
-    email: String;
-    password: String;
+  email: String;
+  password: String;
+};
+export type RegistrationData = {
+  firstname: String;
+  lastname: String;
+  username: String;
+  email: String;
+  password: String;
+  repeat_password: String;
+};
+export type ResendData = {
+  email: String;
 };
