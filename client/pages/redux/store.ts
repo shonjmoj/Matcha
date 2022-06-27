1e4eb417977164226d7066a870c151fa7f997ce7
Miid0 <elounejj@student.1337.ma>
2022-06-27 21:07:01 +0100

    working on registration state

--- client/pages/redux/store.ts
+++ client/pages/redux/store.ts
@@ -1,1 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import registerReducer from "./RegistrationState";
+
+
+export default configureStore ({
+    reducer :{
+        register: registerReducer
+    }
+})
