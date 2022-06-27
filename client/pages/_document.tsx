06293cd5309c5e3f4c96117899e82dd30446c805
aymane <aymensarrar1@gmail.com>
2022-06-27 12:35:32 +0100

    switching the document to a class component to not override the default one

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
