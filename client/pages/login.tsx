41174ecf9f5d8372906ebfc974996110310e1ad0
aymane <aymensarrar1@gmail.com>
2022-06-30 12:54:27 +0100

    fixed small err

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -10,14 +9,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import Modal from "../components/Modal";
 import toast, { Toaster } from "react-hot-toast";
-=======
-import React from 'react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import Joi from 'joi';
-import { LoginData } from '../types/types';
->>>>>>> origin/shonjmoj
 
 export default function Login() {
   const { register, handleSubmit } = useForm<LoginData>();
@@ -26,7 +17,7 @@ export default function Login() {
   const [err, setErr] = useState(false);
   const schema = Joi.object({
     email: Joi.string().email({ tlds: { allow: false } }),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
   });
   useEffect(() => {
     const notify = () => toast(state);
@@ -35,10 +26,10 @@ export default function Login() {
   const onSubmit: SubmitHandler<LoginData> = async (data) => {
     const { error, value } = schema.validate(data);
     if (!error) {
-      const result = await fetch('http://localhost:3001/api/login', {
-        method: 'POST',
+      const result = await fetch("http://localhost:3001/api/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(value),
       });
@@ -63,58 +54,57 @@ export default function Login() {
   };
   return (
     <motion.div
-      className='text-white container w-full sm:w-[50%]'
+      className="text-white container w-full sm:w-[50%]"
       initial={{ x: -600, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
-<<<<<<< HEAD
       transition={{ delay: 0.5 }}
     >
       {state !== "pending" && (
         <Modal title="Registration successful" content={state} />
       )}
-=======
-      transition={{ delay: 0.5 }}>
->>>>>>> origin/shonjmoj
       <motion.h1
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1.5 }}
-        className='mb-8 text-2xl font-semibold text-center xl:text-4xl xl:mb-12'>
+        className="mb-8 text-2xl font-semibold text-center xl:text-4xl xl:mb-12"
+      >
         It is never too late <br /> to fall in love
       </motion.h1>
       <Toaster />
       <form
-        className='flex flex-col items-center gap-2'
-        onSubmit={handleSubmit(onSubmit)}>
-        <label className='flex flex-col font-semibold'>
+        className="flex flex-col items-center gap-2"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label className="flex flex-col font-semibold">
           Username / Email
           <input
-            type='text'
-            {...register('email', { required: true })}
-            className='bg-transparent border-white font-normal border-[1px] outline-none p-2'
+            type="text"
+            {...register("email", { required: true })}
+            className="bg-transparent border-white font-normal border-[1px] outline-none p-2"
           />
         </label>
-        <label className='flex flex-col font-semibold'>
+        <label className="flex flex-col font-semibold">
           Password
           <input
-            type='password'
-            {...register('password', { required: true })}
-            className='bg-transparent border-white font-normal border-[1px] outline-none p-2'
+            type="password"
+            {...register("password", { required: true })}
+            className="bg-transparent border-white font-normal border-[1px] outline-none p-2"
           />
         </label>
-        <span className='text-sm font-light lg:text-md'>
-          You are new here ?{' '}
-          <span className='underline'>
-            <Link href='/registration'>Create an account</Link>
+        <span className="text-sm font-light lg:text-md">
+          You are new here ?{" "}
+          <span className="underline">
+            <Link href="/registration">Create an account</Link>
           </span>
         </span>
         <motion.button
-          type='submit'
-          className='px-20 py-2 mt-2 border-2 shadow-xs hover:shadow-sm hover:shadow-white shadow-white/50'
+          type="submit"
+          className="px-20 py-2 mt-2 border-2 shadow-xs hover:shadow-sm hover:shadow-white shadow-white/50"
           whileTap={{
             scale: 1,
           }}
-          whileHover={{ scale: 1.1 }}>
+          whileHover={{ scale: 1.1 }}
+        >
           Login
         </motion.button>
       </form>
