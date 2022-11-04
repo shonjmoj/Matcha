d60812c8463ceae07f9dd3e5df0168858869dff1
Mehdi <elounejj@student.1337.ma>
2022-11-04 09:47:29 +0100

    display all type of button

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,14 @@
+import { Button } from '../components/common';
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className='grid grid-cols-2 gap-3'>
+      <Button type='primary'>Button</Button>
+      <Button type='primary' disabled>
+        Button
+      </Button>
+      <Button type='secondary'>Button</Button>
+      <Button type='outlined'>Button</Button>
+    </div>
+  );
 }
