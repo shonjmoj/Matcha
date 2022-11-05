b2756e0fd65611c45704fbfddcef52bb4a6fc384
Mehdi <elounejj@student.1337.ma>
2022-11-05 11:53:30 +0100

    feat : add Input component (only text type tested)

---
+++ client/components/common/Input/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { IoWarningOutline } from 'react-icons/io5';
+export interface InputProps {
+  value: string;
+  label: string;
+  type: string;
+  placeholder: string;
+  onChange?: (e: any) => void;
+  error?: string;
+}
+
+export default function index({
+  label,
+  value,
+  type,
+  placeholder,
+  error,
+}: InputProps) {
+  return (
+    <div className='flex flex-col gap-1 text-sm md:text-base'>
+      <label htmlFor='name' className='ml-1'>
+        {label}
+      </label>
+      <input
+        value={value}
+        type={type}
+        name='email'
+        placeholder={placeholder}
+        className={`bg-transparent border ${
+          error
+            ? 'border-2 border-red-600'
+            : 'border-border shadow-border/20 focus:shadow-border'
+        } rounded-md p-2 md:px-3 shadow-sm 
+              outline-none transition-all`}
+      />
+      <span
+        className={`${
+          error ? 'text-red-600 text-sm ml-1' : 'hidden'
+        } flex items-start gap-1`}>
+        <IoWarningOutline />
+        {error}
+      </span>
+    </div>
+  );
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,9 @@
-import { Button } from '../components/common';
+import { Button, Input } from '../components/common';
 
 export default function Home() {
   return (
     <div className='grid grid-cols-2 gap-3'>
-      <Button type='primary'>Button</Button>
-      <Button type='primary' disabled>
-        Button
-      </Button>
-      <Button type='secondary'>Button</Button>
-      <Button type='outlined'>Button</Button>
+      <Input label='Email' type='text' placeholder='test@test.com' />
     </div>
   );
 }
