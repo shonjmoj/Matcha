da976b37f0863772d70d1632efe7fa8a6514eb14
Mehdi <elounejj@student.1337.ma>
2022-11-08 12:04:20 +0100

    feat : add select input

--- client/components/common/Input/index.tsx
+++ client/components/common/Input/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { useMemo } from 'react';
 import { IoWarningOutline } from 'react-icons/io5';
+import Select from 'react-select';
 export interface InputProps {
   value: string;
   label: string;
   type: string;
   placeholder: string;
   onChange?: (e: any) => void;
+  options?: string[];
   error?: string;
 }
 
@@ -14,32 +16,55 @@ export default function index({
   value,
   type,
   placeholder,
+  options,
   error,
 }: InputProps) {
+  const parsedOptions = useMemo(() => {
+    {
+      if (!options) return [];
+
+      return options.map((option, i) => ({
+        label: option,
+        value: i,
+      }));
+    }
+  }, [options]);
+
   return (
     <div className='flex flex-col gap-1 text-sm md:text-base w-[80%] md:w-[30%] lg:w-[20%]'>
-      <label htmlFor='name' className='ml-1'>
-        {label}
-      </label>
-      <input
-        value={value}
-        type={type}
-        name='email'
-        placeholder={placeholder}
-        className={`bg-transparent border ${
-          error
-            ? 'border-2 border-red-600'
-            : 'border-border shadow-border/20 focus:shadow-border'
-        } rounded-md p-2 md:px-3 shadow-sm 
-              outline-none transition-all w-full`}
-      />
-      <span
-        className={`${
-          error ? 'text-red-600 text-sm ml-1' : 'hidden'
-        } flex items-start gap-1`}>
-        <IoWarningOutline />
-        {error}
-      </span>
+      {type === 'select' ? (
+        <>
+          <label htmlFor='name' className='ml-1'>
+            {label}
+          </label>
+          <Select placeholder={placeholder} options={parsedOptions} />
+        </>
+      ) : (
+        <>
+          <label htmlFor='name' className='ml-1'>
+            {label}
+          </label>
+          <input
+            value={value}
+            type={type}
+            name='email'
+            placeholder={placeholder}
+            className={`bg-transparent border ${
+              error
+                ? 'border-2 border-red-600'
+                : 'border-border shadow-border/20 focus:shadow-border'
+            } rounded-md p-2 md:px-3 shadow-sm 
+              outline-none transition-all w-full placeholder:text-gray-600`}
+          />
+          <span
+            className={`${
+              error ? 'text-red-600 text-sm ml-1' : 'hidden'
+            } flex items-start gap-1`}>
+            <IoWarningOutline />
+            {error}
+          </span>
+        </>
+      )}
     </div>
   );
 }
